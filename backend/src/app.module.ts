81a70a5deb7123fd80ab54a65f81f23e2956195b
livinc888 <livinclivinc888@gmail.com>
2026-03-12 07:54:23 +0530

    fix postgres connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentsModule } from './students/students.module';
-import { Student } from './students/student.entity';
 
 @Module({
   imports: [
-
-   TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [Student],
-  synchronize: true,
-}),
-
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
     StudentsModule,
-
   ],
 })
 export class AppModule {}
